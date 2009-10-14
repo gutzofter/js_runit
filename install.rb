a756d1e00e6e178527083646fe21f38ac8e0466a
Joseph Gutierrez <gutzofter@yahoo.com>
2009-10-13 19:55:26 -0700

    gutz: initial unit test push

---
+++ install.rb
@@ -0,0 +1,19 @@
+require 'fileutils'
+
+#Copy the Javascript files
+puts "Copying Javascripts..."
+FileUtils.copy(Dir[File.dirname(__FILE__) + '/javascript/*.js'], File.dirname(__FILE__) + '/../../../public/javascripts/')
+puts "...Done"
+
+#Copy the controller file
+puts "Copying controller..."
+FileUtils.copy(Dir[File.dirname(__FILE__) + '/controller/*.rb'], File.dirname(__FILE__) + '/../../../app/controllers/')
+puts "...Done"
+
+#Copy the view file
+puts "Copying view..."
+dir = File.dirname(__FILE__) + '/../../../app/views/js_runit/'
+Dir.mkdir(dir) 
+FileUtils.copy(Dir[File.dirname(__FILE__) + '/view/js_runit/*.erb'], dir)
+puts "...Done"
+
